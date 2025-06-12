2aa7d99551196a76a9e39055228aa9b98fb13658
ngtienanh <cris@bearabyte.io>
2025-06-13 00:25:25 +0700

    remove

--- src/modules/table/dto/update-table.dto.ts
+++
@@ -1,5 +0,0 @@
-// update-table.dto.ts
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTableDto } from './create-table.dto';
-
-export class UpdateTableDto extends PartialType(CreateTableDto) {}
